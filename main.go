e2345e110fa4d4a15b7b878b229c59eee5bd8071
bmqdev <bmqdev@gmail.com>
2019-06-24 10:47:09 +0700

    go go

--- main.go
+++ main.go
@@ -8,11 +8,32 @@ type Card struct {
 	face   string // S,H,D,C
 }
 
+// S - หน้าโพธิ์ดำ
+// H - หัวใจ
+// D - ข้าวหลามตัด
+// C - ดอกจิก
+
 // PokerHands -
 type PokerHands struct {
 	cards [5]Card
 }
 
 func main() {
-	fmt.Println("kata poker")
+	fmt.Println("kata poker");
+	cards := [5]Card{
+		Card{"A", "S"}, 
+		Card{"A", "C"}, 
+		Card{"A", "H"}, 
+		Card{"A", "D"}, 
+		Card{"", ""}
+	}
+	hand := PokerHands{cards}
+	fmt.Println(hand);
 }
+
+func cal() {
+
+
+	
+}
+
--- main_test.go
+++ main_test.go
@@ -1,1 +1,27 @@
-//
+
+package main
+
+import "testing"
+import "github.com/stretchr/testify/assert"
+
+type Person struct {
+	name string
+}
+
+func TestFrame1DoubleStrike(t *testing.T) {
+	scores := [...] string{
+		"X", "-",
+		"X", "-",
+		"-", "-",
+		"-", "-",
+		"-", "-",
+		"-", "-",
+		"-", "-",
+		"-", "-",
+		"-", "-",
+		"-", "-", "-", 
+	}
+	result := board(scores);
+	expected := int64(30);
+	assert.Equal(t, expected, result[10 - 1].sumScore, "Result should be 30")
+}
