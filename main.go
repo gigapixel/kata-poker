e2c1a2752ccdb44d6943f7aa461ae98096ea5e78
Peerapat <gigapixel7@gmail.com>
2019-06-24 10:00:32 +0700

    Initial files

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// Card -
+type Card struct {
+	points string // 2 - A
+	face   string // S,H,D,C
+}
+
+// PokerHands -
+type PokerHands struct {
+	cards [5]Card
+}
+
+func main() {
+	fmt.Println("kata poker")
+}
