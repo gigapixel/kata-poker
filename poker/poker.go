c64a7f2e93c3fb77b969d9be6eb4b11aaf2bff40
Wanatchapong Bew <bewbangluang@gmail.com>
2019-06-26 00:25:53 +0700

    complete poker

---
+++ poker/poker.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+// Card -
+type Card struct {
+	points string // 2 - A
+	face   string // S(Spade), H(Heart), D(Diamond), C(Club)
+	value  int
+}
+
+// PokerHands -
+type PokerHands struct {
+	cards [5]Card
+}
+
+type ByValue []Card
+
+func (a ByValue) Len() int           { return len(a) }
+func (a ByValue) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByValue) Less(i, j int) bool { return a[i].value < a[j].value }
+
+func getPointsValue(points string) int {
+	if strings.EqualFold("J", points) {
+		return 11
+	} else if strings.EqualFold("Q", points) {
+		return 12
+	} else if strings.EqualFold("K", points) {
+		return 13
+	} else if strings.EqualFold("A", points) {
+		return 14
+	} else {
+		num, err := strconv.Atoi(points)
+		if err != nil {
+			return 0
+		}
+		return num
+	}
+}
+
+func assignPointsValue(cards []Card) {
+	for i := 0; i < len(cards); i++ {
+		cards[i].value = getPointsValue(cards[i].points)
+	}
+}
+
+func groupAndCountByFace(cards []Card) map[string]int {
+	m := map[string]int{}
+	for _, c := range cards {
+		m[c.face]++
+	}
+	return m
+}
+
+func groupAndCountByPoints(cards []Card) map[string]int {
+	m := map[string]int{}
+	for _, c := range cards {
+		m[c.points]++
+	}
+	return m
+}
+
+func isStraight(cards []Card) bool {
+	for i, card := range cards {
+		if i == 0 {
+			continue
+		}
+		if (cards[i-1].value + 1) != card.value {
+			return false
+		}
+	}
+	return true
+}
+
+func isFlush(faces map[string]int) bool {
+	return len(faces) == 1
+}
+
+func printCardsTable(cards []Card) {
+	for _, c := range cards {
+		fmt.Println("Card[", c.face, "][", c.points, "] = ", c.value)
+	}
+}
+
+func poker(cards []Card) interface{} {
+	assignPointsValue(cards)
+	sort.Sort(ByValue(cards))
+
+	faces := groupAndCountByFace(cards)
+	points := groupAndCountByPoints(cards)
+
+	straight := isStraight(cards)
+	flush := isFlush(faces)
+	fourOfaKind := false
+	threeOfaKind := false
+	firstPair := false
+	secondPair := false
+
+	for _, count := range points {
+		if count == 4 {
+			fourOfaKind = true
+		}
+
+		if count == 3 {
+			threeOfaKind = true
+		}
+
+		if count == 2 && !firstPair {
+			firstPair = true
+		} else if count == 2 && !secondPair {
+			secondPair = true
+		}
+	}
+
+	if straight && flush {
+		return "straight flush"
+	}
+
+	if straight {
+		return "straight"
+	}
+
+	if flush {
+		return "flush"
+	}
+
+	if fourOfaKind {
+		return "four of a kind"
+	}
+
+	twoPair := firstPair && secondPair
+	onePair := !twoPair && (firstPair || secondPair)
+
+	if twoPair {
+		return "two pair"
+	}
+
+	if threeOfaKind && onePair {
+		return "full house"
+	}
+
+	if threeOfaKind {
+		return "three of a kind"
+	}
+
+	if onePair {
+		return "one pair"
+	}
+
+	return "high card"
+}
---
+++ poker/poker_test.go
@@ -0,0 +1,112 @@
+package main
+
+import "testing"
+import "github.com/stretchr/testify/assert"
+
+func TestStraightFlush(t *testing.T) {
+	cards := []Card{
+		Card{points: "J", face: "C"},
+		Card{points: "7", face: "C"},
+		Card{points: "9", face: "C"},
+		Card{points: "10", face: "C"},
+		Card{points: "8", face: "C"},
+	}
+	actual := poker(cards)
+	assert.Equal(t, "straight flush", actual, "poker cards on hand should be straight flush")
+}
+
+func TestFourOfKind(t *testing.T) {
+	cards := []Card{
+		Card{points: "5", face: "C"},
+		Card{points: "5", face: "D"},
+		Card{points: "5", face: "H"},
+		Card{points: "5", face: "S"},
+		Card{points: "2", face: "D"},
+	}
+	actual := poker(cards)
+	assert.Equal(t, "four of a kind", actual, "poker cards on hand should be four of kind")
+}
+
+func TestFullHouse(t *testing.T) {
+	cards := []Card{
+		Card{points: "6", face: "S"},
+		Card{points: "6", face: "H"},
+		Card{points: "6", face: "D"},
+		Card{points: "K", face: "C"},
+		Card{points: "K", face: "H"},
+	}
+	actual := poker(cards)
+	assert.Equal(t, "full house", actual, "poker cards on hand should be full house")
+}
+
+func TestFlush(t *testing.T) {
+	cards := []Card{
+		Card{points: "J", face: "D"},
+		Card{points: "9", face: "D"},
+		Card{points: "8", face: "D"},
+		Card{points: "4", face: "D"},
+		Card{points: "3", face: "D"},
+	}
+	actual := poker(cards)
+	assert.Equal(t, "flush", actual, "poker cards on hand should be flush")
+}
+
+func TestStraight(t *testing.T) {
+	cards := []Card{
+		Card{points: "10", face: "D"},
+		Card{points: "9", face: "S"},
+		Card{points: "8", face: "H"},
+		Card{points: "7", face: "D"},
+		Card{points: "6", face: "C"},
+	}
+	actual := poker(cards)
+	assert.Equal(t, "straight", actual, "poker cards on hand should be straight")
+}
+
+func TestThreeOfKind(t *testing.T) {
+	cards := []Card{
+		Card{points: "Q", face: "C"},
+		Card{points: "Q", face: "S"},
+		Card{points: "Q", face: "H"},
+		Card{points: "9", face: "H"},
+		Card{points: "2", face: "S"},
+	}
+	actual := poker(cards)
+	assert.Equal(t, "three of a kind", actual, "poker cards on hand should be three of kind")
+}
+
+func TestTwoPair(t *testing.T) {
+	cards := []Card{
+		Card{points: "J", face: "H"},
+		Card{points: "J", face: "S"},
+		Card{points: "3", face: "C"},
+		Card{points: "3", face: "S"},
+		Card{points: "2", face: "H"},
+	}
+	actual := poker(cards)
+	assert.Equal(t, "two pair", actual, "poker cards on hand should be two pair")
+}
+
+func TestOnePair(t *testing.T) {
+	cards := []Card{
+		Card{points: "10", face: "S"},
+		Card{points: "10", face: "H"},
+		Card{points: "8", face: "S"},
+		Card{points: "7", face: "H"},
+		Card{points: "4", face: "C"},
+	}
+	actual := poker(cards)
+	assert.Equal(t, "one pair", actual, "poker cards on hand should be one pair")
+}
+
+func TestHighCard(t *testing.T) {
+	cards := []Card{
+		Card{points: "K", face: "D"},
+		Card{points: "Q", face: "D"},
+		Card{points: "7", face: "S"},
+		Card{points: "4", face: "S"},
+		Card{points: "3", face: "H"},
+	}
+	actual := poker(cards)
+	assert.Equal(t, "high card", actual, "poker cards on hand should be high card")
+}
