7d955df77abc5be7a3cc2739732c4e5cc451305f
Nopparit <nopparip@scg.com>
2019-06-25 11:17:42 +0700

    add IsContinueNumber function

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 import "sort"
-// import "strconv"
+import "strconv"
 
 // Card -
 type Card struct {
@@ -18,11 +18,11 @@ type PokerHands struct {
 func main() {
 	fmt.Println("kata poker")
 
-	c1 := Card{points: "4", face: "S"}
-	c2 := Card{points: "4", face: "S"}
-	c3 := Card{points: "4", face: "S"}
-	c4 := Card{points: "4", face: "S"}
-	c5 := Card{points: "5", face: "A"}
+	c1 := Card{points: "2", face: "S"}
+	c2 := Card{points: "3", face: "S"}
+	c3 := Card{points: "7", face: "S"}
+	c4 := Card{points: "5", face: "S"}
+	c5 := Card{points: "6", face: "A"}
 
 	pokerHands := PokerHands{};
 	pokerHands.cards[0] = c1;
@@ -30,7 +30,7 @@ func main() {
 	pokerHands.cards[2] = c3;
 	pokerHands.cards[3] = c4;
 	pokerHands.cards[4] = c5;
-	fmt.Println(pokerHands)
+	//fmt.Println(pokerHands)
 	
 	// r1 := SameFace(pokerHands);
 	// fmt.Println("Same Face : ", r1);
@@ -41,7 +41,9 @@ func main() {
 	// 	fmt.Println("Four of a kind");
 	// }
 
-	getOutput(pokerHands);
+	//getOutput(pokerHands);
+
+	IsContinueNumber(pokerHands);
 
 	// fmt.Println("aaaa" , SameNumber(pokerHands));
 	// IsContinueNumber(pokerHands, 3);
@@ -51,36 +53,40 @@ func main() {
 func getOutput(hands PokerHands) {
 	if(IsFlsh(hands)) {
 		fmt.Println("Flsh");
-	} else if(SameNumber(hands) == 4) {
+	} else if(SameNumber(hands, 12) == 4) {
 		fmt.Println("Four of a kind");
+	} else if(isFullhouse(hands)) {
+		fmt.Println("Full house");
 	}
 }
 
-// func IsContinueNumber(hands PokerHands, n) {
-// 	var score []int
-// 	for i := 0; i < 5; i++ {
-// 		if(hands.cards[i].points == "K") {
-// 			score = append(score, 13);
-// 		} else if(hands.cards[i].points == "Q") {
-// 			score = append(score, 12);
-// 		} else if(hands.cards[i].points == "J") {
-// 			score = append(score, 11);
-// 		} else {
-// 			k, _ := strconv.Atoi(hands.cards[i].points)
-//     	score = append(score, k);
-// 		}
-// 	}
+func isFullhouse(hands PokerHands) bool {
+	return SameNumber(hands, 12) == 3 && SameNumber(hands, 11) == 2;
+}
 
-// 	sort.Ints(score)
+func IsContinueNumber(hands PokerHands) {
+	var score []int
+	for i := 0; i < 5; i++ {
+		if(hands.cards[i].points == "K") {
+			score = append(score, 13);
+		} else if(hands.cards[i].points == "Q") {
+			score = append(score, 12);
+		} else if(hands.cards[i].points == "J") {
+			score = append(score, 11);
+		} else {
+			k, _ := strconv.Atoi(hands.cards[i].points)
+    	score = append(score, k);
+		}
+	}
 
-// 	if(score[0] )
+	sort.Ints(score)
 
-// 	for i := 0; i < 5; i++ {
-// 		fmt.Println("score : ", score[i]);
-// 	}
-// }
+	fmt.Println("score[0] : ", score[0] + 4);
+	fmt.Println("score[1] : ", score[4]);
 
-func SameNumber(hands PokerHands) int {
+}
+
+func SameNumber(hands PokerHands, index int) int {
 	var n2 = 0;
 	var n3 = 0;
 	var n4 = 0;
@@ -114,13 +120,13 @@ func SameNumber(hands PokerHands) int {
 			n9++;
 		} else if(hands.cards[i].points == "10") {
 			n10++;
-		} else if(hands.cards[i].points == "11") {
+		} else if(hands.cards[i].points == "J") {
 			n11++;
-		} else if(hands.cards[i].points == "12") {
+		} else if(hands.cards[i].points == "Q") {
 			n12++;
-		} else if(hands.cards[i].points == "13") {
+		} else if(hands.cards[i].points == "K") {
 			n13++;
-		} else if(hands.cards[i].points == "14") {
+		} else if(hands.cards[i].points == "A") {
 			n14++;
 		}
 	}
@@ -128,7 +134,7 @@ func SameNumber(hands PokerHands) int {
 	ints := []int{n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14};
 
 	sort.Ints(ints)
-	return ints[12];
+	return ints[index];
 }
 
 func SameFace(hands PokerHands) int {
@@ -158,8 +164,3 @@ func SameFace(hands PokerHands) int {
 func IsFlsh(hands PokerHands) bool {
 	return SameFace(hands) == 5;
 }
-
-
-// func IsStaightFlsh(cards [5]Card) {
-// 	return true;
-// }
