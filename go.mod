e9a9f40593687e9380565a8367df136061b3f61c
kookoo <koo_g0kek@hotmail.com>
2019-06-25 10:37:13 +0700

    tar

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.12
 
-require github.com/stretchr/testify v1.3.0
+require (
+	github.com/bradfitz/slice v0.0.0-20180809154707-2b758aa73013
+	github.com/stretchr/testify v1.3.0
+)
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "github.com/bradfitz/slice"
 
 // Card -
 type Card struct {
@@ -16,13 +17,18 @@ type PokerHands struct {
 func main() {
 	// fmt.Println("kata poker")
 	card1 := Card{"2", "S"}
-	card2 := Card{"2", "H"}
-	card3 := Card{"2", "D"}
-	card4 := Card{"2", "C"}
-	card5 := Card{"6", "S"}
+	card2 := Card{"3", "S"}
+	card3 := Card{"6", "S"}
+	card4 := Card{"4", "S"}
+	card5 := Card{"2", "S"}
 	// hand := PokerHands(cards)
 	// hand.print(card1)
 	cards := [5]Card{card1, card2, card3, card4, card5}
+	slice.Sort(cards[:], func(i, j int) bool {
+		return cards[i].points < cards[j].points
+	})
+	// fmt.Print(cards)
+
 	pokerHands := PokerHands{cards}
 
 	result := printHand(pokerHands)
@@ -33,20 +39,41 @@ func main() {
 func printHand(hand PokerHands) string {
 	result := ""
 	// pPoint := ""
-	// cPoint := ""
+	countPoint := 1
 	// pFace := ""
 	countFace := 1
 
-	// 4 cards
+	// Straight flush
+
+	// Four of a kind
 	for i := 0; i <= 3; i++ {
 		if hand.cards[i].points == hand.cards[i+1].points {
+			countPoint++
+		}
+	}
+
+	// Full house
+
+	// flush
+	for i := 0; i <= 3; i++ {
+		if hand.cards[i].face == hand.cards[i+1].face {
 			countFace++
 		}
 	}
 
-	if countFace == 4 {
-		result = "Four of a kind"
-	}
+	// Straight
+	// Three of a kind
+	// Two pair
+	// One pair
 
+	if countPoint == 4 {
+		result = "Four of a kind"
+	} else if countFace == 5 {
+		result = "Flush"
+	} else if countPoint == 3 {
+		result = "Three of a kind"
+	} else if countPoint == 2 {
+		result = "One pair"
+	}
 	return result
 }
